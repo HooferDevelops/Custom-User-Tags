5fdde16c94ebeb320c7e00e4ac4f20a25e67b6f4
Hoofer <60201971+HooferDevelops@users.noreply.github.com>
2020-09-23 11:46:26 -0400

    changed to native bot tag system
    basically i'm using the same setup and classes as normal bot tags, looks better and is easier for theme developers

--- components/Tag.jsx
+++ components/Tag.jsx
@@ -3,6 +3,8 @@ const { React } = require('powercord/webpack');
 
 module.exports = ({tagName, classType, style}) => (
   <Tooltip position='top' text={`${tagName}`} className={`${classType}`} delay={200}>
-    <div className='bot-tag-plugin' style={style}>{`${tagName}`}</div>
+    <span style={style} className='botTag-3W9SuW botTagRegular-2HEhHi botTag-2WPJ74 px-10SIf7'>
+      <span className="botText-1526X_">{`${tagName}`}</span>
+    </span>
   </Tooltip>
 );
