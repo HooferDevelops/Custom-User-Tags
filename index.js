02859a966f06342f6b15848267f9208f91ba88b4
HooferDevelops <BatHun0214A@mischools.org>
2021-02-11 20:23:24 -0500

    Revert "yay, tooltips!"
    This reverts commit 052914f0b5177eabdda320ddbab5da9f0ef5aac7.

--- index.js
+++ index.js
@@ -1,30 +1,114 @@
-import React from 'react';
-
-import { Plugin } from '@vizality/entities';
-import { findInReactTree } from '@vizality/util/react';
-import { patch, unpatch } from '@vizality/patcher';
-import { getModule, getModuleByDisplayName, getModules } from '@vizality/webpack';
-
-const label = require('./components/label.jsx');
-
-export default class HoofersFirstPlugin extends Plugin {
-start () {
-    this.injectStyles('style.scss');
-    const MaskedLink = getModuleByDisplayName("MaskedLink", false)
-    patch('title-inject', MaskedLink.prototype, "render", (args, res) => {
-        if (!res.props || !res.props.children)
-            return res;
-        if (typeof(res.props.children) === "object")
-            res.props.children = res.props.children[0];
-
-        if (!res.props.href.startsWith("https://discord"))
-            res.props.children = React.createElement(label, {field:res.props.children,hoverText:res.props.children});
-        
-        return res;
-    });
-  }
-
-  stop () {
-     unpatch('title-inject')
-  }
+const { Plugin } = require('powercord/entities');
+const { React, getModule, getModuleByDisplayName} = require('powercord/webpack');
+const { findInReactTree } = require('powercord/util')
+const { inject, uninject } = require('powercord/injector');
+const Settings = require('./components/Settings');
+const tag = require('./components/Tag.jsx');
+async function getUserProfileBody () {
+    const UserProfile = await getModuleByDisplayName('UserProfile');
+    const VeryVeryDecoratedUserProfileBody = UserProfile.prototype.render().type;
+    const VeryDecoratedUserProfileBody = VeryVeryDecoratedUserProfileBody.prototype.render.call({ memoizedGetStateFromStores: () => void 0 }).type;
+    const DecoratedUserProfileBody = VeryDecoratedUserProfileBody.render().type;
+    return DecoratedUserProfileBody.prototype.render.call({ props: { forwardedRef: null } }).type;
 }
+  
+module.exports = class CustomUserTags extends Plugin {
+    
+
+
+    async startPlugin(){
+
+
+
+        const getSetting = (setting) => this.settings.get(setting);
+        this.loadStylesheet('style.scss')
+        function getUser(id) {
+            var list = getSetting("userTags")
+            return list.find(e=> e.id == id)
+        }
+
+        powercord.api.settings.registerSettings('bot-tag-settings', {
+            category: this.entityID,
+            label: 'User Tags',
+            render: Settings
+        });
+
+        const PrivateChannel = await getModuleByDisplayName('PrivateChannel');
+        const MemberListItem = await getModuleByDisplayName('MemberListItem'); // thanks badges-everywhere
+
+
+
+        const UserProfileBody = await getUserProfileBody();
+        const NameTag = await getModuleByDisplayName('NameTag');
+
+        const MessageTimestamp = await getModule(["MessageTimestamp"]);
+
+
+        inject('bot-tag-message', MessageTimestamp, "default", function(args, res){
+            var id = args[0].message.author.id;
+            var user = getUser(id);
+            if (user != null){
+                res.props.children[1].props.children.join()
+                res.props.children[1].props.children.splice(2, 0, React.createElement(tag,{style: {'margin-left': '0px', 'background': user.background, 'color': user.fontcolor}, tagName: user.tag, classType: `bot-tag-tooltip ${user.animated}`, extra: "botTagCozy-1fFsZk botTag-1un5a6 botTagRegular-2HEhHi botTag-2WPJ74 rem-2m9HGf toprole-wrapper tag"}))
+                 res.props.children[1].props.children.join()
+                
+            }
+            return res;
+        })
+        /*inject('bot-tag-userinfo', UserProfileBody.prototype, 'render', function (args, res) {
+            console.log()
+            var props = res.props.children.props.children[0].props.children[0].props.children[1].props.children[0].props
+            res.props.children.props.children[0].props.children[0].props.children[1].props.children[0].props = []
+            res.props.children.props.children[0].props.children[0].props.children[1].props.children[0].props
+            //res.props.children.props.children[0].props.children[0].props.children[1].props.children[0] = React.createElement(tag, {});
+            console.log(res)
+            return res;
+        })
+        inject('bot-tag-test', NameTag.prototype, 'render', function(args, res){
+            console.log(res)
+            return res;
+        })*/
+        inject('bot-tag-members', MemberListItem.prototype, 'renderDecorators', function(args, res){
+            if (!this.props.user.id) return res;
+            if (!getSetting("extraSettings").showMemberList) return res;
+            var id = this.props.user.id
+            var user = getUser(id);
+            if (user != null){
+                res.props.children[0] = React.createElement(tag,{style: {'background': user.background, 'color': user.fontcolor}, tagName: user.tag, classType: `bot-tag-tooltip ${user.animated}`, extra: ""})
+            } else if (this.props.user.bot == true) {
+                // YOINKED
+                //res.props.children[0] = React.createElement(tag,{tagName: "BOT", classType: "bot-tag-tooltip"})
+            }
+            return res;
+        })
+        inject('bot-tag-sidebar', PrivateChannel.prototype, 'render', function (args, res) {
+            if (!this.props.user) return res;
+            var id = this.props.user.id
+            var user = getUser(id);
+            if (user != null){
+                res.props.name.props.children = [res.props.name.props.children]
+                res.props.name.props.children.push(React.createElement(tag,{style: {'background': user.background, 'color': user.fontcolor}, tagName: user.tag, classType: `bot-tag-tooltip ${user.animated}`, extra: ""}));
+            } else if (this.props.user.bot == true){
+                res.props.name.props.children = [res.props.name.props.children]
+                res.props.name.props.children.push(React.createElement(tag,{tagName: "BOT", classType: "bot-tag-tooltip", extra: ""})); // heck off i'm doing it this way, i don't know a better way help me (っ °Д °;)っ
+            }
+            return res;
+        })
+    }
+
+
+        
+
+
+
+
+    pluginWillUnload(){
+        uninject('bot-tag-message')
+        //uninject('bot-tag-test')
+        uninject('bot-tag-sidebar')
+        uninject('bot-tag-members')
+        //uninject('bot-tag-userinfo')
+        powercord.api.settings.unregisterSettings('bot-tag-settings');
+    }
+
+};
